a641842930c0b82516fce39d751d0dc58d1a3caf
unknown <seldead@mail.ru>
2022-04-21 11:54:59 +0300

    correct

--- src/com/netcracker/chNine/programmingProjects/exTwoThree/WorldClock.java
+++ src/com/netcracker/chNine/programmingProjects/exTwoThree/WorldClock.java
@@ -1,16 +1,7 @@
 package com.netcracker.chNine.programmingProjects.exTwoThree;
 
-public class WorldClock extends Clock{
-
+public class WorldClock extends Clock {
     public WorldClock(int timeOffset) {
         time = time.plusHours(timeOffset);
     }
-
-    @Override
-    public String getHours() {
-        return Integer.toString(time.getHour());
-    }
-
-
-
 }
